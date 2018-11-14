eeb745d88afb663fddcb88a01dbb93aee3c8a457
dandybytes <dandybytes@gmx.com>
2018-11-14 23:45:23 +0200

    first commit: create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>It's alive!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
